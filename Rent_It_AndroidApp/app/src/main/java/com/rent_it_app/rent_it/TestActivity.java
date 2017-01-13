1ff2cde2e8cdea29eef8d3075b80fd5c810e018a
Mizumi Malhan <mmalhan@oakland.edu>
2017-01-13 14:01:56 -0500

    Updated SignUpActivity

---
+++ Rent_It_AndroidApp/app/src/main/java/com/rent_it_app/rent_it/TestActivity.java
@@ -0,0 +1,15 @@
+package com.rent_it_app.rent_it;
+
+import android.os.Bundle;
+
+/**
+ * Created by Nagoya on 1/13/17.
+ */
+
+public class TestActivity extends BaseActivity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
